5ef3f0ff4aa9db7911c2f0a9da3e2d99c8c84e0c
lxin <1830152723@qq.com>
2023-01-04 16:34:52 +0800

    test

---
+++ src/main/java/Hiworld.java
@@ -0,0 +1,10 @@
+/**
+ * @author ：lxin
+ * @date ：Created 2023/1/4 16:26
+ * @description：
+ */
+public class Hiworld {
+    public static void main(String[] args) {
+        System.out.println("Hi  World!");
+    }
+}
